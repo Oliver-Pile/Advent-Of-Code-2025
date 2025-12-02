0fede9e743f75dedf2107c0aeb533dde194dacd5
Oliver Pile <oliver.pile@bt.com>
2025-12-02 12:49:43 +0000

    Day 2 part 2

--- day2/day2.go
+++ day2/day2.go
@@ -11,8 +11,8 @@ func main() {
 	part1Count := part1()
 	part2Count := part2()
 
-	fmt.Printf("%v\n", part1Count)
-	fmt.Printf("%v\n", part2Count)
+	fmt.Printf("Part1: %v\n", part1Count)
+	fmt.Printf("Part2: %v\n", part2Count)
 
 }
 
@@ -40,15 +40,44 @@ func part1() int {
 
 		}
 	}
-
-	sum := 0
-	for _, id := range invalidIDs {
-		sum += id
-	}
-	return sum
+	return sum(invalidIDs)
 
 }
 func part2() int {
+	input, _ := os.ReadFile("day2/day2.txt")
 
-	return 0
+	splitValues := strings.Split(string(input), ",")
+
+	var invalidIDs []int
+	for _, vals := range splitValues {
+		pair := strings.Split(vals, "-")
+		lower, _ := strconv.Atoi(pair[0])
+		upper, _ := strconv.Atoi(pair[1])
+		fmt.Printf("%v-%v\n", lower, upper)
+
+		for i := lower; i <= upper; i++ {
+			strVal := strconv.Itoa(i)
+			strLen := len(strVal)
+			for j := 2; j <= strLen; j++ {
+				partial := strVal[:strLen/j]
+
+				if strings.Repeat(partial, j) == strVal {
+					fmt.Printf("Invalid: %v\n", i)
+					invalidIDs = append(invalidIDs, i)
+					break
+				}
+
+			}
+
+		}
+	}
+	return sum(invalidIDs)
+}
+
+func sum(input []int) int {
+	sum := 0
+	for _, val := range input {
+		sum += val
+	}
+	return sum
 }
