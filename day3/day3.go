dd016be893240c6c7636483e9b54b2c67f4e3dcd
Oliver Pile <oliver.pile@bt.com>
2025-12-03 13:03:44 +0000

    Day 3 part 2

--- day3/day3.go
+++ day3/day3.go
@@ -9,12 +9,8 @@ import (
 )
 
 func main() {
-	file, _ := os.Open("day3/day3.txt")
-	defer file.Close()
 
-	scanner := bufio.NewScanner(file)
-
-	part1Count := part1(scanner)
+	part1Count := part1()
 	part2Count := part2()
 
 	fmt.Printf("Part 1: %v\n", part1Count)
@@ -22,7 +18,11 @@ func main() {
 
 }
 
-func part1(scanner *bufio.Scanner) int {
+func part1() int {
+	file, _ := os.Open("day3/day3.txt")
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
 	fmt.Println("In func")
 
 	var total int
@@ -58,5 +58,33 @@ func findLargest(line string) string {
 }
 
 func part2() int {
-	return 0
+	file, _ := os.Open("day3/day3.txt")
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	var total int
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		result := ""
+		for i := 11; i >= 0; i-- {
+			length := len(line)
+			end := length - i
+			subStr := line[0:end]
+			fmt.Printf("Slice: %s\n", subStr)
+			largest := findLargest(subStr)
+			fmt.Printf("Largest: %s\n", largest)
+			largestIndex := strings.Index(line, largest)
+			line = line[largestIndex+1:]
+			fmt.Printf("New line: %s\n", line)
+			result = fmt.Sprintf("%s%s", result, largest)
+		}
+		fmt.Printf("Result: %s\n", result)
+
+		num, _ := strconv.Atoi(result)
+		total += num
+
+	}
+
+	return total
 }
